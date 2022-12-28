5723e8093e98ee8d06e596851a370a862849a276
BonnyTrade <griwko1@gmail.com>
2022-12-28 10:18:58 +0200

    deploy

---
+++ TokensCreation/BinanceSmartChainToken.sol
@@ -0,0 +1,29 @@
+/* 
+Binance Smart Chain (BEP20) Token Creation Guide List
+
+
+1. Install Metamask if You Don't Have It - https://metamask.io/download/
+
+2. Add BSC MainNet or BSC TestNet To Metamask from ChainList - https://chainlist.org/
+
+3. Send Some Crypto for Your Metamask Wallete to Confirm Transactions Latter 
+    - For MaiNet Chain Send Real Crypto, that You can Buy on Differente Crypto Exchange, for Example ByBit - https://www.bybit.com/ru-RU/invite?ref=WMMJGM
+    - For BSC TestNET you can use this BSC Faucet - https://testnet.bnbchain.org/faucet-smart
+
+4. Create Token Code in OpenZeppelin Wizzard - https://wizard.openzeppelin.com/
+
+5. Import Code From OpenZeppelin to RemixIDE - https://remix.ethereum.org/
+
+6. Put Your Token Contracts to One File In Remix IDE
+
+7. Check That Your Name, Symbol, TotalSupply and Decimals Variables are Right, after Deploing You will Not Change It!
+
+8. Deploy your Token Contract In Remix IDE, of Course Confirm Transaction
+
+9. Check Verify and Publish your Contract on BSC TestNet or MaiNet
+
+Congratulation Everithing Are Done!
+    - Now You can Import Your Tokens To Your Metamask 
+    - Now You can Connect Your Metamask To BSCScan And Use Token Functions
+    - Now You can do with Your Tokens Everything That Allow Blockchain Technologie
+*/
